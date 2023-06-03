6d99b39f645ac70bb4d548a2799f77f83a5400c5
tsurubaso <tsurubaso@gmail.com>
2023-06-03 11:42:15 +0900

    Api part finished

---
+++ WebApplication1a/Controllers/EventsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using WebApplication1a.Models;
+
+namespace WebApplication1a.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        public EventsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+        [HttpPost]
+        [Route("AddEvent")]
+
+        public Response AddEvent(Events events)
+        {
+            Response response = new Response();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("SNCon").ToString());
+            Dal dal = new Dal();
+            response = dal.AddEvent(events, connection);
+            return response;
+        }
+
+        [HttpGet]
+        [Route("EventList")]
+
+        public Response EventList()
+        {
+            Response response = new Response();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("SNCon").ToString());
+            Dal dal = new Dal();
+            response = dal.EventsList(connection);
+            return response;
+        }
+
+
+
+
+
+
+
+    }
+}
